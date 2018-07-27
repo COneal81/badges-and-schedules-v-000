87f685439ac1a06660daa486cfdf4779d590bf29
Kari O'Neal <triadfirewood@gmail.com>
2018-07-27 21:29:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+  
+
+def batch_bage_creator(attendees)
+  attendees.each do |attendees|
+  return "Hello, my name is #{attendees}"
+  end
+end
+  
+  
+  def assign_rooms(speaker)
+  end
