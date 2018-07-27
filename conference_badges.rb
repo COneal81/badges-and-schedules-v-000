43829c552a96ec45d872655bee7b0887340ba470
Kari O'Neal <triadfirewood@gmail.com>
2018-07-27 22:15:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,34 +1,3 @@
 # Write your code here.
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-end
-
-
-def batch_bage_creator(attendees)
-  attendees.collect do |name|
-    badge_maker(name)
-    end
-  end
-  
-  
-  def assign_rooms(attendees)
-    room = 1
-    attendees.collect do |name|
-    puts"Hello, #{name}!  You'll be assigned to room #{room}"
-    room += 1
-  end
-end
-  
-  def printer (attendees)
-   batch_bage_creator.each do |value|
-     puts value
-  end
-  assign_rooms.each do |value|
-    puts value
-  end
-end
-  
-  
   
