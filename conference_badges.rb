621e65a9ddca68f31432cf2be972ca8cc0846bf9
Kari O'Neal <triadfirewood@gmail.com>
2018-07-27 22:18:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,10 +14,11 @@ def batch_bage_creator(attendees)
   
   
   def assign_rooms(attendees)
-    room = 1
+    room = 0
     attendees.collect do |name|
-    puts"Hello, #{name}!  You'll be assigned to room #{room}"
     room += 1
+    "Hello, #{name}!  You'll be assigned to room #{room}"
+    
   end
 end
   
